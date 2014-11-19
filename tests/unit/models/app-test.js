1b56d1efc10f45826f0a1a8a43784f24e3dd7b06
Cory Forsyth <cory.forsyth@gmail.com>
2014-11-19 10:48:06 -0700

    remove pretender in app unit test

--- tests/unit/models/app-test.js
+++ tests/unit/models/app-test.js
@@ -2,10 +2,9 @@ import {
   moduleForModel,
   test
 } from 'ember-qunit';
+import { stubRequest } from "../../helpers/fake-server";
 
 import Ember from 'ember';
-
-import Pretender from 'pretender';
 
 var server;
 
@@ -21,14 +20,12 @@ test('creating POSTs to correct url', function(){
     return store.createRecord('app', {handle:'my-cool-app', account: {id:'1'}});
   });
 
-  server = new Pretender(function(){
-    this.post('/accounts/1/apps', function(request){
-      ok(true, 'calls with correct URL');
+  stubRequest('post', '/accounts/1/apps', function(request){
+    ok(true, 'calls with correct URL');
 
-      return [200, {}, {
-        id: 'my-app-id',
-        handle: 'my-cool-app'
-      }];
+    return this.success(201, {
+      id: 'my-app-id',
+      handle: 'my-cool-app'
     });
   });
 
