8df2f40fc48f8f8de8bdd3ae99874e6a2b75306d
Cory Forsyth <cory.forsyth@gmail.com>
2015-01-12 11:55:04 -0500

    check for boolean properties in aptible-ability assertions

--- app/components/aptible-ability/component.js
+++ app/components/aptible-ability/component.js
@@ -3,7 +3,7 @@ import can from '../../utils/can';
 
 export default Ember.Component.extend({
   hasAbility: false, // cannot use `isVisible` with `tagName: ''`
-  tagName: '',
+  tagName: '', // tagName must be exactly '' to render this component with no surrounding tag
 
   user: null,
   scope: null,
@@ -15,9 +15,9 @@ export default Ember.Component.extend({
         stack = this.get('stack'),
         component = this;
 
-     Ember.assert("You must provide a user to aptible-ability", user);
-     Ember.assert("You must provide a scope to aptible-ability", scope);
-     Ember.assert("You must provide a stack to aptible-ability", stack);
+     Ember.assert("You must provide a user to aptible-ability", !!user);
+     Ember.assert("You must provide a scope to aptible-ability", !!scope);
+     Ember.assert("You must provide a stack to aptible-ability", !!stack);
 
      can(user, scope, stack).then(function(bool){
        if (component.isDestroyed) { return; }
