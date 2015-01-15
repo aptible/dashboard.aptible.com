e26eceb34d5ebf32c49ee9db083c95c14dd8c812
Cory Forsyth <cory.forsyth@gmail.com>
2015-01-15 12:07:15 -0500

    add tests for /stack/:id/apps

---
+++ tests/acceptance/apps-test.js
@@ -0,0 +1,55 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+import { stubRequest } from '../helpers/fake-server';
+
+var App, server;
+
+module('Acceptance: Apps', {
+  setup: function() {
+    App = startApp();
+    stubStacks({includeApps:true});
+  },
+  teardown: function() {
+    Ember.run(App, 'destroy');
+    if (server) {
+      server.shutdown();
+    }
+  }
+});
+
+test('visiting /stacks/1/apps requires authentication', function() {
+  expectRequiresAuthentication('/stacks/1/apps');
+});
+
+test('visiting /stacks/1/apps', function() {
+  signInAndVisit('/stacks/1/apps');
+
+  andThen(function() {
+    equal(currentPath(), 'stacks.stack.apps.index');
+  });
+});
+
+test('visiting /stacks/1/apps shows list of apps', function() {
+  signInAndVisit('/stacks/1/apps');
+
+  andThen(function() {
+    var appRows = find('.panel.app');
+
+    equal(appRows.length, 2, '2 apps');
+  });
+});
+
+test('visiting /stacks/1/apps then clicking on an app visits the app', function() {
+  signInAndVisit('/stacks/1/apps');
+
+  andThen(function(){
+    var appLink = find('a[href~="/apps/1"]');
+    ok(appLink.length, 'has link to app 1');
+
+    click(appLink);
+  });
+
+  andThen(function(){
+    equal(currentPath(), 'app.index', 'app show page is visited');
+  });
+});
