c2f862ba6ae211d0b5a33ac3ca769d62b2acc128
Cory Forsyth <cory.forsyth@gmail.com>
2015-01-27 11:29:14 -0500

    User Settings SSH, profile and admin pages
      * User can add/delete ssh keys (/settings/ssh)
      * User can change password, email (/settings/admin)
      * User can change name (/settings/profile)
      * Styles match dashboard
      * Customized ssh-key adapter's buildURL for DELETEs
      * pin ember-data-hal-adapter to 0.1.3 (0.1.4 is broken)
      * Use model hook instead of session.currentUser in settings pages
    
    fixes #66 fixes #69

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -5,7 +5,7 @@ import {
 
 moduleForModel('user', 'User', {
   // Specify the other units that are required for this test.
-  needs: ['model:token', 'model:role', 'model:organization']
+  needs: ['model:token', 'model:role', 'model:organization','model:ssh-key']
 });
 
 test('it exists', function() {
