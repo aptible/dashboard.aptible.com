09490c0fb844a3f9aa5a3854b3de7a41ba5c6f5d
Cory Forsyth <cory.forsyth@gmail.com>
2015-01-28 11:31:28 -0500

    Remove commented-out tests in ssh-key unit test

--- tests/unit/models/ssh-key-test.js
+++ tests/unit/models/ssh-key-test.js
@@ -21,68 +21,14 @@ moduleForModel('ssh-key', 'SshKey', {
 });
 
 /*
-test('can FIND ssh-key by id', function(){
-  var store = this.store();
-
-  stubRequest('get', '/ssh_keys/1', function(){
-    return this.success({
-      id: '1'
-    });
-  });
-
-  return Ember.run(function(){
-    return store.find('ssh-key',1).then(function(key){
-      ok(!!key, 'finds key');
-      equal(key.get('id'),'1','has id');
-    });
-  });
-});
-
-
-test('when creating an sshKey for a user, POSTs to /users/:user_id/ssh_keys', function(){
-  expect(6);
-
-  var store = this.store();
-
-  var key, user;
-  var userId = 'my-user-id';
-  var keyName = 'the-key';
-  var sshPublicKey = 'rsa-ssh ABCSDFWEFSGS';
-
-
-  Ember.run(function(){
-    user = store.createRecord('user', {id: userId});
-    key = store.createRecord('ssh-key', {
-      name: keyName,
-      sshPublicKey: sshPublicKey,
-      user: user
-    });
-  });
-
-  stubRequest('post', '/users/' + userId + '/ssh_keys', function(request){
-    ok(true, 'posts to correct url');
-    var json = JSON.parse(request.requestBody);
-
-    equal(json.name, keyName, 'has key name in json');
-    equal(json.ssh_public_key, sshPublicKey, 'has public_ssh_key in json');
-
-    return this.success({
-      id: 'key-id',
-      name: keyName,
-      public_key_fingerprint: 'abcdef'
-    });
-  });
-
-  return Ember.run(function(){
-    return key.save().then(function(){
-      ok(true, 'key saved');
-
-      equal(key.get('id'), 'key-id', 'key has correct id');
-      equal(key.get('publicKeyFingerprint'), 'abcdef', 'key has fingerprint');
-    }).catch(function(e){
-      console.log('error:',e);
-      ok(false, 'error:'+e);
-    });
-  });
-});
-*/
+ * There is a bug in the interaction between ember data and the
+ * isolated container used in `moduleForModel` unit tests
+ * that causes issues unit-testing this ssh-key model.
+ * The store will end up incorrectly unable to find the model
+ * when it calls `modelFor('sshKey')`.
+ * More details are here: https://github.com/bantic/data/pull/1
+ *
+ * There should be tests here that POSTing and DELETEing ssh keys
+ * use the correct urls (/users/:user_id/ssh_keys and /ssh_keys/:ssh_key_id
+ * for the latter, respectively).
+ */
