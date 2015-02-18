36e8d6519d0d82a7129f3829615cd9543925936c
Skylar Anderson <skylar@aptible.com>
2015-02-18 15:36:28 -0500

    adding some tests, adding organization settings link to each organization

--- app/stacks/index/route.js
+++ app/stacks/index/route.js
@@ -4,9 +4,11 @@ export default Ember.Route.extend({
   redirect: function(){
     var stacks = this.modelFor('stacks');
     var stack = stacks.objectAt(0);
-    if (stack) {
-      this.replaceWith('apps', stack);
-    } else {
+
+    if(stacks.get('length') === 0) {
       this.replaceWith('welcome.first-app');
+    } else if(stacks.get('length') === 1) {
+      this.replaceWith('apps', stack);
     }
+  }
 });
--- app/stacks/route.js
+++ app/stacks/route.js
@@ -1,11 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  afterModel: function(stacks) {
-    if(stacks.get('length') === 1) {
-      this.transitionTo('stack.apps.index', stacks.get('firstObject'));
-    }
-  },
   model: function(){
     return this.store.find('stack');
   }
