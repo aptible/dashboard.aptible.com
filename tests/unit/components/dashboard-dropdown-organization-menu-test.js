11625750b43aa9f2e6f157bfbe1cbea94fb05f19
Skylar Anderson <skylar@aptible.com>
2015-02-19 14:30:43 -0500

    adds organization settings links to user nav

---
+++ tests/unit/components/dashboard-dropdown-organization-menu-test.js
@@ -0,0 +1,22 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+import Ember from 'ember';
+
+moduleForComponent('dashboard-dropdown-organization-menu', 'DashboardDropdownOrganizationMenuComponent', {
+  needs: ['component:dashboard-dropdown-item', 'component:link-to-legacy-organization']
+});
+
+test('it renders', function() {
+  // creates the component instance
+  var component = this.subject({
+    organization: Ember.Object.create({ name: 'Foundry', id: '123' })
+  });
+  equal(component._state, 'preRender');
+
+  // appends the component to the page
+  this.render();
+  equal(component._state, 'inDOM');
+});
