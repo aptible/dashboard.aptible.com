e9d0b543f558214fdf96c0266e4e1f56660b7a0e
Skylar Anderson <skylar@aptible.com>
2015-02-27 16:58:09 -0500

    added save progress component spec

--- app/components/save-progress/component.js
+++ app/components/save-progress/component.js
@@ -5,14 +5,12 @@ export default Ember.Component.extend({
   classNames: ['save-progress'],
   hasProgress: Ember.computed.gt('progress.currentStep', 0),
   showProgressBar: Ember.computed.and('hasProgress', 'incomplete'),
-
-  incomplete: function() {
-    return this.get('progress.currentStep') < this.get('progress.totalSteps');
-  }.property('progress.currentStep', 'progress.totalSteps'),
+  incomplete: Ember.computed.lt('progressPercent', 100),
 
   progressPercent: function() {
     var current = this.get('progress.currentStep');
     var total = this.get('progress.totalSteps');
+
     return (current/total) * 100;
   }.property('progress.totalSteps', 'progress.currentStep'),
 
---
+++ tests/unit/components/save-progress-test.js
@@ -0,0 +1,41 @@
+import {
+  moduleForComponent,
+  test
+} from 'ember-qunit';
+
+import startApp from '../../helpers/start-app';
+import Ember from 'ember';
+
+moduleForComponent('save-progress', 'SaveProgressComponent', {});
+
+test('it renders', function() {
+  expect(2);
+
+  var progress = Ember.Object.create({ totalSteps: 3, currentStep: 0});
+  var component = this.subject({ progress: progress });
+
+  equal(component._state, 'preRender');
+
+  this.render();
+  equal(component._state, 'inDOM');
+});
+
+test('it updates loading bar', function() {
+  var progress = Ember.Object.create({ totalSteps: 4, currentStep: 0});
+  var component = this.subject({ progress: progress });
+
+  equal(component.get('showProgressBar'), false, 'Progress bar is hidden when current step is 0');
+  equal(component.get('incomplete'), true, 'Progress bar is incomplete when not started');
+
+  progress.set('currentStep', 1);
+
+  equal(component.get('showProgressBar'), true, 'Progress bar is visible when current step is > 0');
+  equal(component.get('incomplete'), true, 'Progress bar is incomplete');
+  equal(component.get('progressPercent'), 25, 'Progress is 25 percent complete');
+
+  progress.set('currentStep', 4);
+
+  equal(component.get('showProgressBar'), false, 'Progress bar is hidden when all steps complete');
+  equal(component.get('incomplete'), false, 'Progress bar is complete');
+  equal(component.get('progressPercent'), 100, 'Progress is 100 percent complete');
+});
