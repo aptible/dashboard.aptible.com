dd3403d8b15f236bf847f94c7e6ce47881a40fce
Conrad VanLandingham <cv@utexas.edu>
2015-04-01 21:35:25 -0500

    Force-show error messages when submitting signup form

--- app/components/validation-feedback/component.js
+++ app/components/validation-feedback/component.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'span',
-  classNames: ['input-group-addon']
+  classNames: ['input-group-addon'],
+
+  /** @type {Boolean} Whether errors will show even if no value */
+  hasSubmitted: false,
+
+  shouldShowErrors: Ember.computed.or("value", "hasSubmitted"),
+
+  errorText: function() {
+    var error = this.get("error");
+
+    // If multiple errors, join errors with space and comma
+    if(Array.isArray(error) && error.join) {
+      error = error.join(", ");
+    }
+
+    return error;
+  }.property("error", "error.@each")
 });
 
