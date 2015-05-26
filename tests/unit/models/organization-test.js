44ed33f6c43106621406b3c4d8746666a5106532
Matthew Beale <matt.beale@madhatted.com>
2015-05-26 12:06:17 -0400

    Add allowPHI property to organization models

--- tests/unit/models/organization-test.js
+++ tests/unit/models/organization-test.js
@@ -3,6 +3,7 @@ import {
   test
 } from 'ember-qunit';
 import modelDeps from '../../support/common-model-dependencies';
+import Ember from "ember";
 
 moduleForModel('organization', 'model:organization', {
   // Specify the other units that are required for this test.
@@ -13,4 +14,20 @@ test('it exists', function() {
   var model = this.subject();
   // var store = this.store();
   ok(!!model);
+});
+
+test('it allowsPHI based on plan', function() {
+  var model = this.subject();
+  Ember.run(() => {
+    model.set('plan', 'development');
+  });
+  ok(!model.get('allowPHI'), 'PHI not allowed');
+  Ember.run(() => {
+    model.set('plan', 'production');
+  });
+  ok(model.get('allowPHI'), 'PHI allowed');
+  Ember.run(() => {
+    model.set('plan', 'platform');
+  });
+  ok(model.get('allowPHI'), 'PHI allowed');
 });
