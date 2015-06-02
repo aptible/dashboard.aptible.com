185b1f3abbd486f84286c98f41ccac39ed7e24a2
Cory Forsyth <cory.forsyth@gmail.com>
2015-06-02 17:57:13 -0400

    Delete test helpers that ember-cli-aptible-shared provides

--- tests/helpers/set-feature.js
+++
@@ -1,8 +0,0 @@
-import Ember from "ember";
-
-export default function setFeature(app, featureName, value) {
-  var service = app.__container__.lookup('features:-main');
-  service[value ? 'enable' : 'disable'](featureName);
-}
-
-Ember.Test.registerHelper('setFeature', setFeature);
