f4178bb478e3b264c5bc03d97e68d9d1e11e4b53
Skylar Anderson <skylar@anderson.is>
2015-06-02 15:58:03 -0400

    fixes aptible/dashboard.aptible.com#314

--- app/components/with-active-class/component.js
+++ app/components/with-active-class/component.js
@@ -6,7 +6,6 @@ export default Ember.Component.extend({
   isActive: Ember.computed('route', 'applicationController.currentPath', function() {
     var currentPath = this.applicationController.get('currentPath');
     var route = this.get('route');
-    return currentPath.indexOf(route) === 0;
+    return currentPath.indexOf(route) > -1;
   })
-
 });
