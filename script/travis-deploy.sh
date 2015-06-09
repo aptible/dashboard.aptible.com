ba2e0375eef495cf4a3b487167b2a10a59f6007c
Frank Macreery <frank@macreery.com>
2015-06-09 18:41:01 -0400

    Migrate to S3/CloudFront deployment

---
+++ script/travis-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Deploy to staging/production on master/release merges (not PRs)
+
+set -e
+
+# Don't deploy on PRs
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  # Deploy to staging on a merge to master
+  ember build -e staging
+  ember deploy -e staging
+elif [ "$TRAVIS_BRANCH" == "release" ]; then
+  # Deploy to production on a merge to release
+  ember build -e production
+  ember deploy -e production
+fi
