787f4cbb7abd087419405191286ce7d88a4ccee5
Skylar Anderson <skylar@aptible.com>
2015-06-23 13:52:26 -0400

    remove support for legacy dashboard

--- app/organization/route.js
+++ app/organization/route.js
@@ -1,18 +1,6 @@
 import Ember from 'ember';
-import Location from '../utils/location';
-import config from '../config/environment';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    var params = this.paramsFor('organization');
-    return this._super.apply(this, arguments).then(() => {
-      if (!this.features.isEnabled('organization-settings')) {
-        var host = config.aptibleHosts['legacy-dashboard'];
-        var url = [host, 'organizations', params.organization_id].join('/');
-        return Location.replaceAndWait(url);
-      }
-    });
-  },
   renderTemplate() {
     this._super.apply(this, arguments);
     this.render('sidebars/organization', {
