029791c795dd0fae9834134c55ef3751723e4466
Robert Jackson <robert.w.jackson@me.com>
2015-07-09 18:53:46 -0400

    Refactor config/deploy.js.
    * Make a little more DRY
    * Provide helpful error messages when missing required ENV variables.
    * Allow customizing `AWS_BUCKET` (allows deployment to newly built
      CloudFormation stacks).

--- config/deploy.js
+++ config/deploy.js
@@ -1,41 +1,49 @@
-module.exports = {
-  production: {
-    buildEnv: 'production',
+var SilentError = require('silent-error');
 
-    store: {
-      type: 's3-static',
-      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
-      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      acl: 'public-read',
-      bucket: 'dashboard.aptible.com'
-    },
-
-    assets: {
-      type: 's3',
-      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
-      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      acl: 'public-read',
-      bucket: 'dashboard.aptible.com'
-    }
-  },
-
-  staging: {
-    buildEnv: 'staging',
-
-    store: {
-      type: 's3-static',
-      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
-      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      acl: 'public-read',
-      bucket: 'dashboard.aptible-staging.com'
-    },
-
-    assets: {
-      type: 's3',
-      accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
-      secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
-      acl: 'public-read',
-      bucket: 'dashboard.aptible-staging.com'
-    }
-  }
+var storeBucketMap = {
+  production: 'dashboard.aptible.com',
+  development: 'dashboard.aptible-staging.com'
 };
+
+function defaultOptions() {
+}
+
+function optionsFor(environment, type) {
+  var accessKeyId = process.env['AWS_ACCESS_KEY_ID'];
+  var secretAccessKey = process.env['AWS_SECRET_ACCESS_KEY'];
+  var bucket = process.env['AWS_BUCKET'] || storeBucketMap[environment];
+
+  if (!bucket) {
+    throw new SilentError('No bucket was found for ' + environment);
+  }
+
+  if (!accessKeyId) {
+    throw new SilentError('AWS_ACCESS_KEY_ID was not found in `process.env`. Aborting.');
+  }
+
+  if (!secretAccessKey) {
+    throw new SilentError('AWS_SECRET_ACCESS_KEY was not found in `process.env`. Aborting.');
+  }
+
+  return {
+    type: type,
+    accessKeyId: process.env['AWS_ACCESS_KEY_ID'],
+    secretAccessKey: process.env['AWS_SECRET_ACCESS_KEY'],
+    acl: 'public-read',
+    bucket: bucket
+  };
+}
+
+var options = {};
+var environments = ['production', 'staging'];
+
+environments.forEach(function(environment) {
+  options[environment] = {
+    buildEnv: environment,
+
+    store: optionsFor(environment, 's3-static'),
+    assets: optionsFor(environment, 's3')
+  };
+});
+
+module.exports = options;
