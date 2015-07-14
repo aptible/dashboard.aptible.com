bb1085eb554feb902e51745cf54a0c6b3c33ef7e
Robert Jackson <robert.w.jackson@me.com>
2015-07-14 16:05:02 -0400

    Allow ProvisionableMixin to reload while provisioning.

--- tests/unit/models/vhost-test.js
+++ tests/unit/models/vhost-test.js
@@ -6,6 +6,9 @@ import { stubRequest } from 'ember-cli-fake-server';
 import modelDeps from '../../support/common-model-dependencies';
 import Ember from 'ember';
 
+const { run } = Ember;
+const TEST_RELOAD_RETRY_DELAY = 10;
+
 moduleForModel('vhost', 'model:vhost', {
   // Specify the other units that are required for this test.
   needs: modelDeps.concat([
@@ -61,5 +64,42 @@ test('updating PUTs to correct url', function(assert) {
   Ember.run(() => {
     vhost.set('virtualDomain', 'new-virtual-domain.com');
     vhost.save().finally(done);
+  });
+});
+
+test('reloads while provisioning', function(assert) {
+  let done = assert.async();
+  assert.expect(2);
+
+  let store = this.store();
+  let vhost, service;
+  let vhostId = 'vhost-id';
+
+  stubRequest('get', `/vhosts/${vhostId}`, function(request){
+    assert.ok(true, 'calls with correct URL');
+
+    return this.success({
+      id: vhostId,
+      status: 'provisioning'
+    });
+  });
+
+  run(() => {
+    service = store.createRecord('service', {id: '1'});
+    vhost = store.push('vhost', {
+      id: vhostId,
+      status: 'provisioning',
+      service
+    });
+
+    vhost.set('_reloadRetryDelay', TEST_RELOAD_RETRY_DELAY);
+  });
+
+  stubRequest('get', `/vhosts/${vhostId}`, function(request){
+    assert.ok(true, 'calls with correct URL');
+
+    done();
+
+    return this.success({ id: vhostId, status: 'provisioned' });
   });
 });
