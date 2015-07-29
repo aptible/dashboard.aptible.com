301f91698317d5ec4805a8302e263386c4046cff
Robert Jackson <robert.w.jackson@me.com>
2015-07-28 22:05:24 -0400

    Update ember-cli-flash to fix deprecation messages.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -9,13 +9,11 @@ window.deprecationWorkflow.config = {
     { handler: "silence", matchMessage: "`lookup` was called on a Registry. The `initializer` API no longer receives a container, and you should use an `instanceInitializer` to look up objects from the container." },
     { handler: "silence", matchMessage: "Ember.View is deprecated. Consult the Deprecations Guide for a migration strategy." },
     { handler: "silence", matchMessage: /Accessing 'template' in <diesel@component:bs-alert-dismiss/ },
-    { handler: "silence", matchMessage: /Accessing 'template' in <diesel@component:flash-message/ },
     { handler: "silence", matchMessage: "Ember.Select is deprecated. Consult the Deprecations Guide for a migration strategy." },
     { handler: "silence", matchMessage: "A container should only be created for an already instantiated registry. For backward compatibility, an isolated registry will be instantiated just for this container." },
     { handler: "silence", matchMessage: "In Ember 2.0 service factories must have an `isServiceFactory` property set to true. You registered (unknown mixin) as a service factory. Either add the `isServiceFactory` property to this factory or extend from Ember.Service." },
     { handler: "silence", matchMessage: "A container should only be created for an already instantiated registry. For backward compatibility, an isolated registry will be instantiated just for this container." },
     { handler: "silence", matchMessage: "Using the `{{view \"string\"}}` helper is deprecated. ('diesel/app/vhosts/new/template.hbs' @ L20:C12) " },
-    { handler: "silence", matchMessage: "The `bind-attr` helper ('diesel/templates/components/flash-message.hbs' @ L7:C39) is deprecated in favor of HTMLBars-style bound attributes." },
     { handler: "silence", matchMessage: "Using `{{view \"select\"}}` is deprecated. ('diesel/templates/components/user-select.hbs' @ L1:C0) " },
   ]
 };
