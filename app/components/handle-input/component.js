e6325243de330987628a94a3715fb907aad5aa69
Blake Pettersson <blake.pettersson@gmail.com>
2015-08-06 10:43:16 +0200

    CR Fixes.

--- app/components/handle-input/component.js
+++ app/components/handle-input/component.js
@@ -1,4 +1,4 @@
-import Ember from 'ember';
+import OneWayInput from '../one-way-input/component';
 import Autofocusable from '../../mixins/views/autofocusable';
 
 export var maxChars = 64;
@@ -15,45 +15,13 @@ let capitalLetters = /[A-Z]/g;
 let spaces = /\s+/g;
 let nonAlphaNumerics = /[^0-9a-z._-]/g;
 
-function sanitizeInput(input){
-  if (typeof input !== 'string') { return input; }
+export default OneWayInput.extend(Autofocusable, {
+  sanitizeInput: function(input) {
+    if (typeof input !== 'string') { return input; }
 
-  return truncate(input, maxChars).
-         replace(capitalLetters, replaceWithLower).
-         replace(spaces, '-').
-         replace(nonAlphaNumerics, '');
-}
-
-function handleChangeEvent() {
-  let value = this.readDOMAttr('value');
-
-  processValue.call(this, value);
-}
-
-function processValue(rawValue) {
-  let value = sanitizeInput(rawValue);
-
-  if (this._sanitizedValue !== value) {
-    this._sanitizedValue = value;
-    this.attrs.update(value);
-  }
-}
-
-export default Ember.Component.extend(Autofocusable, {
-  tagName: 'input',
-  attributeBindings: [ 'type', 'value', 'placeholder', 'name' ],
-  type: 'text',
-
-  input: handleChangeEvent,
-  change: handleChangeEvent,
-
-  _sanitizedValue: undefined,
-
-  didReceiveAttrs: function() {
-    if (!this.attrs.update) {
-      throw new Error('You must provide an `update` action to `{{handle-input}}`.');
-    }
-
-    processValue.call(this, this.get('value'));
+    return truncate(input, maxChars).
+      replace(capitalLetters, replaceWithLower).
+      replace(spaces, '-').
+      replace(nonAlphaNumerics, '');
   }
 });
--- app/components/one-way-input/component.js
+++ app/components/one-way-input/component.js
@@ -5,25 +5,31 @@ function handleChangeEvent() {
   processValue.call(this, value);
 }
 
-function processValue(value) {
-  if (this._value !== value) {
-    this._value = value;
+function processValue(rawValue) {
+  let value = this.sanitizeInput(rawValue);
+
+  if (this._sanitizedValue !== value) {
+    this._sanitizedValue = value;
     this.attrs.update(value);
   }
 }
 
 export default Ember.Component.extend({
   tagName: 'input',
-  attributeBindings: [ 'type', 'value', 'placeholder', 'data-stripe' ],
+  attributeBindings: [ 'type', 'value', 'placeholder', 'data-stripe', 'name' ],
   type: 'text',
-  _value: undefined,
+  _sanitizedValue: undefined,
 
   input: handleChangeEvent,
   change: handleChangeEvent,
 
+  sanitizeInput: function(input) {
+    return input;
+  },
+
   didReceiveAttrs: function() {
     if (!this.attrs.update) {
-      throw new Error('You must provide an `update` action to `{{stripe-input}}`.');
+      throw new Error(`You must provide an \`update\` action to \`{{${this.templateName}}}\`.`);
     }
 
     processValue.call(this, this.get('value'));
