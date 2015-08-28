b7502de177a9faa6726c7bbed913530e863f9e6f
Skylar Anderson <skylar@aptible.com>
2015-08-28 09:27:12 -0400

    Resolve permittables before checking if role is permitted

--- app/utils/can.js
+++ app/utils/can.js
@@ -1,17 +1,23 @@
 import Ember from 'ember';
 
-export default function can(user, scope, permittable){
-  Ember.assert('Must pass a parameter that implements `permitsRole`',
-               !!permittable.permitsRole);
-
-  if (scope === "read" || user.get('verified')) {
-    return user.get('roles').then(function(roles){
-      return Ember.RSVP.all(roles.map((role) => permittable.permitsRole(role, scope)) );
-    }).then((results) => {
-      return results.indexOf(true) > -1;
-    });
-  } else {
+export default function can(user, scope, permittablePromise){
+  if(scope === 'manage' && !user.get('verified')) {
     return Ember.RSVP.resolve(false);
   }
+
+  let permittable;
+
+  return new Ember.RSVP.Promise(function(resolve) {
+    // Ensure permittable is resolved as a model and not a promise
+    resolve(permittablePromise);
+  }).then(function(resolvedPermittable) {
+    Ember.assert('Must pass a parameter that implements `permitsRole`', !!resolvedPermittable.permitsRole);
+    permittable = resolvedPermittable;
+    return user.get('roles');
+  }).then(function(roles){
+    return Ember.RSVP.all(roles.map((role) => permittable.permitsRole(role, scope)) );
+  }).then((results) => {
+    return results.indexOf(true) > -1;
+  });
 
 }
