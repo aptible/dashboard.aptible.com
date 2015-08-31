d1a8e8f4543ac82875a0a491bcc12184b782bfaf
Robert Jackson <robert.w.jackson@me.com>
2015-08-31 12:17:31 -0400

    Update to ember-resolver via addon.
    This change will be the default in Ember CLI 2.0.0, just getting a head
    start...

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
+import Resolver from 'ember-resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 import AuthenticatedRouteMixin from 'diesel/mixins/routes/authenticated';
