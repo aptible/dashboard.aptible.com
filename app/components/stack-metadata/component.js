126ed3551ba4942c77fc7bc4a1d33c304d3b0465
Skylar Anderson <skylar@anderson.is>
2015-09-18 10:53:07 -0400

    add list of domains to stack header helps with #435

--- app/components/stack-metadata/component.js
+++ app/components/stack-metadata/component.js
@@ -3,9 +3,34 @@ import sum from '../../utils/sum';
 
 export default Ember.Component.extend({
   tagName: '',
+  maxVisibleDomainNames: 2,
 
   persistedApps: Ember.computed.filterBy('model.apps', 'isNew', false),
   persistedDatabases: Ember.computed.filterBy('model.databases', 'isNew', false),
+  persistedVhosts: Ember.computed.filterBy('model.vhosts', 'isNew', false),
+  vhostNames: Ember.computed.mapBy('persistedVhosts', 'commonName'),
+
+  displayVhostNames: Ember.computed('vhostNames', function() {
+    return this.get('vhostNames').join(', ');
+  }),
+
+  showVhostTooltip: Ember.computed('vhostNames', function() {
+    return this.get('vhostNames.length') > this.get('maxVisibleDomainNames');
+  }),
+
+  vhostRemaining: Ember.computed('vhostNames', function() {
+    return this.get('vhostNames.length') - this.get('maxVisibleDomainNames');
+  }),
+
+  vhostNamesSnippet: Ember.computed('vhostNames', function() {
+    let names = this.get('vhostNames');
+    return names.slice(0, this.get('maxVisibleDomainNames')).join(', ');
+  }),
+
+  vhostNamesTooltip: Ember.computed('vhostNames', function() {
+    let names = this.get('vhostNames');
+    return names.slice(this.get('maxVisibleDomainNames')).join(', ');
+  }),
 
   totalDiskSize: function(){
     let sizes = this.get('persistedDatabases').mapBy('disk.size').compact();
