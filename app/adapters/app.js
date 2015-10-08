2628b460a3ddaae0e9d2ac47cd250666fba24e62
Skylar Anderson <skylar@aptible.com>
2015-10-08 12:31:56 -0400

    add findQuery to all stack resources

--- app/adapters/app.js
+++ app/adapters/app.js
@@ -1,14 +1,1 @@
-import StackResource from './stack-resource';
-
-export default StackResource.extend({
-  findQuery: function(store, type, query) {
-    let url = this.buildURL(type.modelName, null, null, 'findQuery');
-
-    if (query.stack) {
-      url = url.replace('/apps', `/accounts/${query.stack.id}/apps`);
-      delete query.stack;
-    }
-
-    return this.ajax(url, 'GET', {data:query});
-  }
-});
+export { default } from './stack-resource';
