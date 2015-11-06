f32d3188c92f451dfdffde96470d560670ca2765
Skylar Anderson <skylar@aptible.com>
2015-11-06 10:04:58 -0500

    running ember-cli-aptible-shared blueprint

--- Brocfile.js
+++ Brocfile.js
@@ -29,8 +29,10 @@ var app = new EmberApp(options);
 app.import('bower_components/nouislider/src/jquery.nouislider.css');
 app.import('bower_components/nouislider/distribute/jquery.nouislider.js');
 
-// tooltips, popovers
-app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  // tooltips, popovers
+app.import('bower_components/bootstrap-sass/assets/javascripts/bootstrap.js');
+app.import('bower_components/bootstrap-sass/assets/stylesheets/_bootstrap.scss');
+
 app.import('bower_components/animate/animate.css');
 app.import("bower_components/font-awesome/css/font-awesome.css");
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.eot", { destDir: "fonts" });
@@ -39,11 +41,8 @@ app.import("bower_components/font-awesome/fonts/fontawesome-webfont.ttf", { dest
 app.import("bower_components/font-awesome/fonts/fontawesome-webfont.woff", { destDir: "fonts" });
 app.import("bower_components/font-awesome/fonts/FontAwesome.otf", { destDir: "fonts" });
 
-app.import("bower_components/zeroclipboard/dist/ZeroClipboard.js");
-app.import("bower_components/zeroclipboard/dist/ZeroClipboard.swf", {
-  destDir: "assets"
-});
-
+// Copy to clipboard
+app.import("bower_components/clipboard/dist/clipboard.js");
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
