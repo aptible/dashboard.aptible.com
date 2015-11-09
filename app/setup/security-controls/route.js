3d675eb92c217121f21852fcc3710c85313d06d8
Skylar Anderson <skylar@anderson.is>
2015-11-09 14:00:23 -0600

    load data environment schemas in security controls

--- app/setup/security-controls/route.js
+++ app/setup/security-controls/route.js
@@ -1,4 +1,95 @@
 import Ember from 'ember';
 
+// For now, just import schemas statically and map the selected DEs and providers
+// When Gridiron is able to serve schemas, this will be replaced with API calls
+// to load previous step's selections
+import aptibleSchema from 'sheriff/schemas/providers/aptible'
+import awsSchema from 'sheriff/schemas/providers/aws'
+import googleSchema from 'sheriff/schemas/providers/google'
+import ebsSchema from 'sheriff/schemas/data-environments/ebs'
+import ec2Schema from 'sheriff/schemas/data-environments/ec2'
+import elbSchema from 'sheriff/schemas/data-environments/elb'
+import glacierSchema from 'sheriff/schemas/data-environments/glacier'
+import gmailSchema from 'sheriff/schemas/data-environments/gmail'
+import googleDocsSchema from 'sheriff/schemas/data-environments/google-docs'
+import googleDriveSchema from 'sheriff/schemas/data-environments/google-drive'
+import laptopsSchema from 'sheriff/schemas/data-environments/laptops'
+import onPremiseSchema from 'sheriff/schemas/data-environments/on-premise'
+import paperSchema from 'sheriff/schemas/data-environments/paper'
+import phoneSchema from 'sheriff/schemas/data-environments/phone'
+import redshiftSchema from 'sheriff/schemas/data-environments/redshift'
+import s3Schema from 'sheriff/schemas/data-environments/s3'
+import usbSchema from 'sheriff/schemas/data-environments/usb'
+import workstationsSchema from 'sheriff/schemas/data-environments/workstations'
+export var schemaMap = {
+  providers: {
+    aptible: aptibleSchema,
+    aws: awsSchema,
+    google: googleSchema
+  },
+  dataEnvironments: {
+    ebs: ebsSchema,
+    ec2: ec2Schema,
+    glacier: glacierSchema,
+    gmail: gmailSchema,
+    'google-docs': googleDocsSchema,
+    'google-drive': googleDriveSchema,
+    laptops: laptopsSchema,
+    'on-premise': onPremiseSchema,
+    paper: paperSchema,
+    phone: phoneSchema,
+    redshift: redshiftSchema,
+    s3: s3Schema,
+    usb: usbSchema,
+    workstations: workstationsSchema
+  }
+};
+
+function selectedProviders(dataEnvironments) {
+  // From a set of provided DEs, determine which providers are being used
+  return dataEnvironments
+         .filter((d) => d.provider)
+         .map((d) => d.provider)
+         .uniq()
+         .without('aptible');
+}
+
+function selectedDataEnvironmentsToSchemaArray(dataEnvironments) {
+  // Given a collection of data environments, collect DEs by provider, flatten,
+  // and map to imported schemas
+  let requiredSchemas = [];
+  let providers = selectedProviders(dataEnvironments);
+
+    providers.forEach((provider) => {
+      let providerDataEnvironments = dataEnvironments.filter((de) => {
+        de.provider == provider
+      }).map((de) => {
+        schemaMap.dataEnvironments[de.handle];
+      });
+
+      requiredSchemas.push(schemaMap.providers[provider]);
+      requiredSchemas.concat(providerDataEnvironments)
+    });
+
+  return requiredSchemas;
+}
+
 export default Ember.Route.extend({
+  beforeModel() {
+    let profile = this.modelFor('setup');
+    let selectedDataEnvironments = profile.get('selectedDataEnvironments');
+    debugger;
+    if(!selectedDataEnvironments || !selectedDataEnvironments.length) {
+      return this.transitionTo('setup.data-environments');
+    }
+  },
+  model() {
+    let profile = this.modelFor('setup');
+    let selectedDataEnvironments = profile.get('selectedDataEnvironments');
+
+    let schemas =  selectedDataEnvironmentsToSchemaArray(selectedDataEnvironments)
+    debugger;
+    return schemas
+  }
 });
+
--- tests/acceptance/setup/security-controls-test.js
+++ tests/acceptance/setup/security-controls-test.js
@@ -2,8 +2,35 @@ import Ember from 'ember';
 import { module, test, skip } from 'qunit';
 import startApp from 'sheriff/tests/helpers/start-app';
 import { stubRequest } from 'ember-cli-fake-server';
+import { orgId, rolesHref, usersHref, invitationsHref, securityOfficerId,
+         securityOfficerHref } from '../../helpers/organization-stub';
 
 let application;
+let securityControlsUrl = `${orgId}/setup/security-controls`;
+let roleId = 'owners-role';
+let userId = 'u1';
+let roles = [
+  {
+    id: roleId,
+    privileged: true,
+    name: 'Owners',
+    _links: {
+      self: { href: `/roles/${roleId}` },
+      users: { href: `/roles/${roleId}/users`}
+    }
+  }
+];
+
+let users = [
+  {
+    id: userId,
+    name: 'Basic User',
+    email: 'basicuser@asdf.com',
+    _links: {
+      self: { href: `/users/${userId}` }
+    }
+  }
+];
 
 module('Acceptance: Setup: Security Controls', {
   beforeEach() {
@@ -15,7 +42,53 @@ module('Acceptance: Setup: Security Controls', {
   }
 });
 
+let selectedDataEnvironments = [
+  { name: 'Aptible PaaS', provider: 'aptible', handle: 'aptible', selected: true },
+  { name: 'EC2', provider: 'aws', handle: 'ec2', selected: false },
+  { name: 'S3', provider: 'aws', handle: 's3', selected: false },
+  { name: 'Google Drive', provider: 'google', handle: 'google-drive', selected: false },
+  { name: 'Workstations', provider: false, handle: 'workstations', selected: false },
+];
+
+test('Selected data environments are used to draw security control questionnaire', function(assert) {
+  stubProfile({ currentStep: 'security-controls', selectedDataEnvironments });
+  stubRequests();
+  signInAndVisit(securityControlsUrl);
+
+  andThen(() => {
+    return pauseTest();
+    // Should show Aptible Provider questions
+    // Should show AWS Provider questions
+    // Should show Google Provider questions
+    // Should show EC2 and S3 Data Environment questions
+    // Should show Google Drive Data Environment questions
+    // Should show provider-less (Workstations) Data Environment questions
+  });
+
+});
+
 skip('Uses a schema document to show security control questions for each data environment');
 skip('Uses a schema document to show security control questions for each provider');
 skip('Uses a schema document to show global security control questions');
+skip('With no data environments configured it redirects back to data environment step');
 skip('Clicking continue saves attestation for each data environment');
+
+function stubRequests() {
+  stubValidOrganization();
+
+  stubRequest('get', rolesHref, function(request) {
+    return this.success({ _embedded: { roles } });
+  });
+
+  stubRequest('get', usersHref, function(request) {
+    return this.success({ _embedded: { users }});
+  });
+
+  stubRequest('get', invitationsHref, function(request) {
+    return this.success({ _embedded: { invitations: [] }});
+  });
+
+  stubRequest('get', securityOfficerHref, function(request) {
+    return this.success(users[0]);
+  });
+}
