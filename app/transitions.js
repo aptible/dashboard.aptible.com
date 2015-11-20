3a2a823a3a1725ccc05c2512d8798dc498f39bbc
Skylar Anderson <skylar@aptible.com>
2015-11-20 10:53:11 -0500

    cleanup transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,51 +1,38 @@
-export default function(){
-  // this.transition(
-  //   this.fromRoute('organization.setup.start'),
-  //   this.toRoute('organization.setup.organization'),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
-
-  // this.transition(
-  //   this.fromRoute('organization.setup.organization'),
-  //   this.toRoute('organization.setup.locations'),
-  //   this.use('toLeft'),
-  //   this.reverse('toRight')
-  // );
+export default function() {
   this.transition(
     this.fromRoute('setup.start'),
     this.toRoute('setup.organization'),
     this.use('toLeft'),
-    this.debug()
+    this.reverse('toRight')
   );
   this.transition(
     this.fromRoute('setup.organization'),
     this.toRoute('setup.locations'),
     this.use('toLeft'),
-    this.debug()
+    this.reverse('toRight')
   );
   this.transition(
     this.fromRoute('setup.locations'),
     this.toRoute('setup.team'),
     this.use('toLeft'),
-    this.debug()
+    this.reverse('toRight')
   );
   this.transition(
     this.fromRoute('setup.team'),
     this.toRoute('setup.data-environments'),
     this.use('toLeft'),
-    this.debug()
+    this.reverse('toRight')
   );
   this.transition(
     this.fromRoute('setup.data-environments'),
     this.toRoute('setup.security-controls'),
     this.use('toLeft'),
-    this.debug()
+    this.reverse('toRight')
   );
   this.transition(
     this.fromRoute('setup.security-controls'),
     this.toRoute('setup.finish'),
     this.use('toLeft'),
-    this.debug()
+    this.reverse('toRight')
   );
 };
