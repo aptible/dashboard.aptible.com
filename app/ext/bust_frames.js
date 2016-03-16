25e5c792844f6ebf76b7b243514d52205b0559c7
gib <gib.reimschussel@gmail.com>
2016-03-16 11:53:30 -0600

    minor cleanup

--- app/ext/bust_frames.js
+++ app/ext/bust_frames.js
@@ -1,5 +1,6 @@
-import config from '../config/environment';
-
+//
+// Dashboard should not be allowed in frames on other sites
+//
 export default function bustFrames() {
   if (top.location !== location) {
     top.location.href = document.location.href;
