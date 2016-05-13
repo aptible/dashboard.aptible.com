6bbcb56515aadef2ac3bafff0a6cc1ebc625a84a
gib <gib.reimschussel@gmail.com>
2016-05-13 01:52:00 -0600

    Indicate total member of a role
    A rendered role summary only includes 5 of the total members. This adds
    “+ n more” for roles with more than five total members.

--- tests/unit/components/user-gravatars-test.js
+++ tests/unit/components/user-gravatars-test.js
@@ -44,3 +44,41 @@ test('it truncates users when passed a count', function(assert) {
 
   assert.equal(component.get('truncatedUsers.length'), 2, 'count of 2 truncates to 2 users');
 });
+
+test('it displays the remainder when the number of users exceeds the count', function(assert) {
+  let users = Ember.A([
+    Ember.Object.create({ email: 'test@example.com'}),
+    Ember.Object.create({ email: 'test1@example.com'}),
+    Ember.Object.create({ email: 'test2@example.com'}),
+    Ember.Object.create({ email: 'test3@example.com'}),
+  ]);
+
+  this.subject({
+    users: users,
+    size: 24,
+    count: 2,
+    placeholder: '--'
+  });
+
+  this.render();
+
+  assert.equal($('.user-gravatar-remainder:contains("+ 2 more")').length, 1, 'displays there are 2 more users');
+});
+
+test('it does not show a remainder when the users are less than or equal to the a count', function(assert) {
+  let users = Ember.A([
+    Ember.Object.create({ email: 'test@example.com'}),
+    Ember.Object.create({ email: 'test1@example.com'})
+  ]);
+
+  this.subject({
+    users: users,
+    size: 24,
+    count: 2,
+    placeholder: '--'
+  });
+
+  this.render();
+
+  assert.equal($('.user-gravatar-remainder').length, 0, 'no remainder of users');
+});
