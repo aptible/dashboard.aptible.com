d84d2fc7356deb887da313b8434229eabd68631f
Thomas Orozco <thomas@orozco.fr>
2016-05-20 14:09:39 +0200

    Improve test coverage on elevation

--- tests/acceptance/elevation-test.js
+++ tests/acceptance/elevation-test.js
@@ -77,3 +77,36 @@ test("Persisting an elevated token does not kill the UI", function(assert) {
   andThen(() => { visit("/"); });
   andThen(() => { assert.equal(currentTokenRequestCount, 1); });
 });
+
+test("Submitting the elevation form requires an elevated token, and does not destroy the current token", function(assert) {
+  assert.expect(6);
+
+  const userEmail = "foo@example.com";
+  const userPassword = "bar";
+  const user = createStubUser({ email: userEmail, password: userPassword });
+
+  let createTokenRequestCount = 0;
+
+  stubRequest('post', '/tokens', function(request) {
+    const params = this.json(request);
+    assert.ok(!request.withCredentials, "The token will not be persisted to cookies");
+    assert.equal(params.username, userEmail, "Email is valid");
+    assert.equal(params.password, userPassword, "Password is valid");
+    assert.equal(params.scope, "elevated", "Scope is elevated");
+    assert.ok(params.expires_in < 3600, "Token expires in less than an hour");
+    createTokenRequestCount++;
+    return this.success(createStubToken({ scope: "elevated" }, user));
+  });
+
+  signIn(user, {}, { scope: "manage" });
+
+  andThen(() => { visit("/settings/protected/admin"); });
+  andThen(() => {
+    fillInput("password", userPassword);
+    clickButton("Confirm");
+  });
+
+  andThen(() => {
+    assert.equal(createTokenRequestCount, 1, "A single request was made to create a token");
+  });
+});
