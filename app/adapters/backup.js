f753ac62964ae71cf71c265c3ba7deda90fc0647
Thomas Orozco <thomas@orozco.fr>
2016-06-13 18:06:10 +0200

    Add database backup model

---
+++ app/adapters/backup.js
@@ -0,0 +1,25 @@
+import ApplicationAdapter from './application';
+import buildURLWithPrefixMap from '../utils/build-url-with-prefix-map';
+
+export default ApplicationAdapter.extend({
+  buildURL: buildURLWithPrefixMap({
+    'databases':   {property: 'database.id', only:['create', 'index']}
+  }),
+
+  findQuery: function(store, type, query){
+    // TODO: type should be a string here?
+    let url = this.buildURL(type.modelName, null, null, 'findQuery');
+
+    if (this.sortQueryParams) {
+      query = this.sortQueryParams(query);
+    }
+
+    if (query.database) {
+      url = url.replace('/backups', `/databases/${query.database.id}/backups`);
+      delete query.database;
+    }
+
+    return this.ajax(url, 'GET', {data: query});
+  }
+});
+
