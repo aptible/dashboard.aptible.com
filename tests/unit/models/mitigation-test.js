98fa7bc7cc469b58c1a519356d6cf3707df7393f
Skylar Anderson <skylar@aptible.com>
2016-06-27 15:46:37 -0700

    model component tests

--- tests/unit/models/mitigation-test.js
+++ tests/unit/models/mitigation-test.js
@@ -4,7 +4,7 @@ import {
 } from 'ember-qunit';
 import modelDeps from '../../support/common-model-dependencies';
 
-moduleForModel('risk-assessment', 'model:risk-assessment', {
+moduleForModel('mitigation', 'model:mitigation', {
   needs: modelDeps
 });
 
