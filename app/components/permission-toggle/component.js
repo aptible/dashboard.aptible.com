2c7c5ee6e34f92a83c4ed42386c35b08a11f3814
gib <gib.reimschussel@gmail.com>
2016-07-07 16:40:20 -0600

    Fix permission toggle component for environments
    Thanks @sandersonet for the help on this.

--- app/components/permission-toggle/component.js
+++ app/components/permission-toggle/component.js
@@ -1,23 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  busy: false,
   hasPermissions: Ember.computed.gt('permissions.length', 0),
+  initialized: false,
   permission: null,
+  store: Ember.inject.service(),
 
-  init() {
-    let stack = this.get('stack');
-    let role = this.get('role');
-    let scope = this.get('scope');
-
-    stack.permissionForRole(role, scope).then(function(permission) {
-      this.set('permission', permission);
-      console.log(permission, permission.get('scope'));
+  setup: function() {
+    Ember.run.next(() => {
+      this.set('initialized', true);
     });
-  },
+  }.on('init'),
 
-  isChecked: Ember.computed('stack.@each.permissions', 'permission.scope', function() {
+  isChecked: Ember.computed('stack.@each.permissions', function() {
     if (this.get('role').get('requiresPermissions')) {
-      return this.get('stack').permitsRole(this.get('role'), this.get('scope'));
+      return this.get('stack').hasRoleScope(this.get('role'), this.get('scope'));
     }
     return true;
   }),
@@ -30,7 +28,34 @@ export default Ember.Component.extend({
     return true;
   }),
 
-  onToggle(permission) {
-    permission.save();
+  actions: {
+    onToggle(isOn) {
+      // NOTE: This version of the toggle component calls onToggle on init,
+      // so we return if that's the case.
+      if (!this.get('initialized') || this.get('busy')) { return; }
+
+      let role = this.get('role');
+      let scope = this.get('scope');
+      let stack = this.get('stack');
+      let permission = stack.findPermission(role, scope);
+
+      // NOTE: No error occurs if a permission with this role and scope already
+      // exist for this stack. Including this check to avoid a duplicate.
+      if (isOn && permission === undefined) { return; }
+
+      this.set('busy', true);
+      if (isOn) {
+        this.get('store').createRecord('permission', {
+          stack,
+          scope,
+          role: role.get('data.links.self')
+        }).save().then(() => { this.set('busy', false); });
+      }
+      else {
+        permission.destroyRecord().then(() => {
+          this.set('busy', false);
+        });
+      }
+    }
   }
 });
