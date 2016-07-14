5e7e8a3ec9af27ebeead2424e247d2e9a4b1c11b
gib <gib.reimschussel@gmail.com>
2016-07-14 14:07:13 -0600

    Role Show Test Stubs
    - Remove role / show
    - Stub tests for members, environments, and settings (completing in a
    follow up TODO)

--- tests/acceptance/role/environments-test.js
+++ tests/acceptance/role/environments-test.js
@@ -4,16 +4,16 @@ import {
   test
 } from 'qunit';
 import startApp from 'diesel/tests/helpers/start-app';
-import { stubRequest } from '../../helpers/fake-server';
+// import { stubRequest } from '../../helpers/fake-server';
 
 let application;
-let orgId = 'o1'; // FIXME this is hardcoded to match the value for signIn in aptible-helpers
+// let orgId = 'o1'; // FIXME this is hardcoded to match the value for signIn in aptible-helpers
 let roleId = 'r1';
-let roleName = 'the-role';
+// let roleName = 'the-role';
 let url = `/roles/${roleId}/members`;
-let apiRoleUrl = `/roles/${roleId}`;
-let apiRoleUsersUrl = `/roles/${roleId}/users`;
-let apiUsersUrl = `/organizations/${orgId}/users`;
+// let apiRoleUrl = `/roles/${roleId}`;
+// let apiRoleUsersUrl = `/roles/${roleId}/users`;
+// let apiUsersUrl = `/organizations/${orgId}/users`;
 
 module('Acceptance: Role Environments', {
   beforeEach: function() {
--- tests/acceptance/role/settings-test.js
+++ tests/acceptance/role/settings-test.js
@@ -4,18 +4,18 @@ import {
   test
 } from 'qunit';
 import startApp from 'diesel/tests/helpers/start-app';
-import { stubRequest } from '../../helpers/fake-server';
+// import { stubRequest } from '../../helpers/fake-server';
 
 let application;
-let orgId = 'o1';
+// let orgId = 'o1';
 let roleId = 'r1';
-let roleName = 'the-role';
+// let roleName = 'the-role';
 let url = `/roles/${roleId}/members`;
-let apiRoleUrl = `/roles/${roleId}`;
-let apiRoleUsersUrl = `/roles/${roleId}/users`;
-let apiUsersUrl = `/organizations/${orgId}/users`;
+// let apiRoleUrl = `/roles/${roleId}`;
+// let apiRoleUsersUrl = `/roles/${roleId}/users`;
+// let apiUsersUrl = `/organizations/${orgId}/users`;
 
-module('Acceptance: Role Members', {
+module('Acceptance: Role Settings', {
   beforeEach: function() {
     application = startApp();
     stubOrganizations();
