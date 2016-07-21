b83bdb41d8ee568f0aa49102828a9d40e586020e
gib <gib.reimschussel@gmail.com>
2016-07-21 14:10:14 -0600

    App Navigation checks current user roles
    Adds logic to render top app navigation according to the current user’s
    roles.

--- app/components/app-navigation/component.js
+++ app/components/app-navigation/component.js
@@ -10,10 +10,31 @@ export default Ember.Component.extend({
     return config.featureFlags['sheriff'];
   }),
 
+  hasAccessToPlatform: Ember.computed('currentUser.roles', function() {
+    let currentUser = this.get('currentUser');
+    if (!currentUser) { return false; }
+    let currentUserRoles = currentUser.get('roles');
+    return currentUserRoles.filterBy('isPlatform').length > 0;
+  }),
+
+  hasAccessToCompliance: Ember.computed('currentUser.roles', function() {
+    let currentUser = this.get('currentUser');
+    if (!currentUser) { return false; }
+    let currentUserRoles = currentUser.get('roles');
+    return currentUserRoles.filterBy('isCompliance').length > 0;
+  }),
+
   sheriffActive: Ember.computed('routingService.currentPath', function() {
     let currentPath = this.get('routingService.currentPath');
     return /^compliance\.compliance-organization/.test(currentPath);
   }),
 
-  opsActive: Ember.computed.not('sheriffActive')
+  opsActive: Ember.computed('routingService.currentPath', function() {
+    return !this.get('sheriffActive') && !this.get('settingsActive');
+  }),
+
+  settingsActive: Ember.computed('routingService.currentPath', function() {
+    let currentPath = this.get('routingService.currentPath');
+    return /members|contact|billing|settings|roles|invite|environments/.test(currentPath);
+  })
 });
