a7224c1b4275bd0dde25cac9c5e622c1b506fc1d
Skylar Anderson <skylar@aptible.com>
2016-07-27 14:56:57 -0700

    show role type nav if on compliance plan

--- app/components/role-type-nav/component.js
+++ app/components/role-type-nav/component.js
@@ -1,20 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-  showNav: Ember.computed('currentUser.roles.[]', 'billingDetail.hasCompliancePlan', function() {
-    const organizationUrl = this.get('organization.data.links.self');
-    let platformRoles = [];
-    let complianceRoles = [];
-    let roles = this.get('currentUserRoles');
-
-    roles.filterBy('data.links.organization', organizationUrl).forEach((role) => {
-      if (role.get('isPlatformOwner') || role.get('isPlatformUser')) {
-        platformRoles.push(role);
-      }
-      if (role.get('isComplianceOwner') || role.get('isComplianceUser')) {
-        complianceRoles.push(role);
-      }
-    });
-    return platformRoles.length > 0 && complianceRoles.length > 0 && this.get('billingDetail.hasCompliancePlan');
-  })
-});
+export default Ember.Component.extend({});
