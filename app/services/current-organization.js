a4d5e5fd3b480bc55cacd7afb99e9b693ea054a3
Skylar Anderson <skylar@aptible.com>
2016-08-10 09:26:19 -0700

    update current organization to use compliance status service

--- app/services/current-organization.js
+++ app/services/current-organization.js
@@ -1,9 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  complianceStatus: Ember.inject.service(),
   organizationHref: Ember.computed(function() {
-    // organizationRout is injected here in an initializer.
-    // This seems wack, but I don't know a better way.
-    return this.get('organizationRoute').modelFor('compliance-organization').get('data.links.self')
+    return this.get('complianceStatus.organization.data.links.self');
   })
 });
