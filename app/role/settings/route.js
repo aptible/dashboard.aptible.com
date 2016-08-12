35710b26fc8641e5ffe82b285c6d301657a5cfda
Skylar Anderson <skylar@aptible.com>
2016-08-12 13:12:07 -0700

    remove promise catch on role settings, [fixes #692]

--- app/role/settings/route.js
+++ app/role/settings/route.js
@@ -5,13 +5,7 @@ export default Ember.Route.extend({
     save() {
       let role = this.currentModel;
       if (role.get('isDirty')) {
-        role.save().catch((e) => {
-          // Silence invalidation errors
-          if (!(e instanceof DS.InvalidError)) {
-            throw e;
-          }
-        }).then(() => {
-          let role = this.currentModel;
+        role.save().then(() => {
           let message = `${role.get('name')} saved`;
 
           Ember.get(this, 'flashMessages').success(message);
