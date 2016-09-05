dca9be8051d945ae581c30f1a6c4fdec6739f7f5
Skylar Anderson <skylar@aptible.com>
2016-09-05 09:34:11 -0700

    remove initial loading indicator in an initializer rather than route

---
+++ app/initializers/loading-indicator.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export function initialize(container, application) {
+  Ember.$(".initial-loading-indicator").remove();
+}
+
--- app/loading/route.js
+++ app/loading/route.js
@@ -1,10 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
-  activate() {
-    // Once 
-    Ember.run.later(() => {
-      Ember.$(".initial-loading-indicator").remove();
-    });
-  }
-});
+export default Ember.Route.extend({});
