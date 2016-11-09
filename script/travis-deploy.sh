6f5769bf9c4420a3006d0faa62ca337f126007cc
Thomas Orozco <thomas@orozco.fr>
2016-11-09 15:01:47 +0100

    Deploy to test on merge to master

--- script/travis-deploy.sh
+++ script/travis-deploy.sh
@@ -11,6 +11,7 @@ fi
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   # Deploy to staging on a merge to master
   ember deploy -e staging
+  DOMAIN=aptible-sandbox.com SUBDOMAIN_SUFFIX=test ember deploy -e sandbox
 elif [ "$TRAVIS_BRANCH" == "release" ]; then
   # Deploy to production on a merge to release
   ember deploy -e production
